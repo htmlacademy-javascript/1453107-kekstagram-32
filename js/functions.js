09f5edb5d610b9a339ff81bd5538c3887564e14d
Mila <darlingmila@yandex.ru>
2024-06-24 16:21:52 +0300

    Добавит функцию для проверки длины строки

---
+++ js/functions.js
@@ -0,0 +1,12 @@
+
+
+const checkStringLength = (str, maxLength) => str.length <= maxLength;
+
+console.group();
+// Строка короче 20 символов
+console.log(checkStringLength('проверяемая строка', 20)); // true
+// Длина строки ровно 18 символов
+console.log(checkStringLength('проверяемая строка', 18)); // true
+// Строка длиннее 10 символов
+console.log(checkStringLength('проверяемая строка', 10)); // false
+console.groupEnd();
