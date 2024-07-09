c5bd1c8c5e492202e07139aacaa5e45c7e67e4be
Mila <darlingmila@yandex.ru>
2024-07-09 14:22:26 +0300

    Оборачивает код по созданию превьюшек в функцию

--- js/main.js
+++ js/main.js
@@ -1,1 +1,5 @@
-import './thumbnails.js';
+import { showThumbnails } from './thumbnails.js';
+
+const MAX_AMOUNT_PHOTOS = 25;
+
+showThumbnails(MAX_AMOUNT_PHOTOS);
--- js/thumbnails.js
+++ js/thumbnails.js
@@ -1,29 +1,30 @@
 import { createArrayOfObjects } from './util.js';
 import { getPhotoObj } from './data.js';
 
-const MAX_AMOUNT_PHOTOS = 25;
-
 const gallery = document.querySelector('.pictures');
 const thumbnailTemplate = document
   .querySelector('#picture')
   .content
   .querySelector('.picture');
 
-const thumbnailsArray = createArrayOfObjects(MAX_AMOUNT_PHOTOS, getPhotoObj);
-const thumbnailsGalleryFragment = document.createDocumentFragment();
+const showThumbnails = (amount) => {
+  const thumbnailsArray = createArrayOfObjects(amount, getPhotoObj);
+  const thumbnailsGalleryFragment = document.createDocumentFragment();
 
+  thumbnailsArray.forEach(({url, description, likes, comments}) => {
+    const newThumbnail = thumbnailTemplate.cloneNode(true);
 
-thumbnailsArray.forEach(({url, description, likes, comments}) => {
-  const newThumbnail = thumbnailTemplate.cloneNode(true);
+    const picture = newThumbnail.querySelector('.picture__img');
+    picture.src = url;
+    picture.alt = description;
 
-  const picture = newThumbnail.querySelector('.picture__img');
-  picture.src = url;
-  picture.alt = description;
+    newThumbnail.querySelector('.picture__likes').textContent = likes;
+    newThumbnail.querySelector('.picture__comments').textContent = comments.length;
 
-  newThumbnail.querySelector('.picture__likes').textContent = likes;
-  newThumbnail.querySelector('.picture__comments').textContent = comments.length;
+    thumbnailsGalleryFragment.append(newThumbnail);
+  });
 
-  thumbnailsGalleryFragment.append(newThumbnail);
-});
+  gallery.append(thumbnailsGalleryFragment);
+};
 
-gallery.append(thumbnailsGalleryFragment);
+export { showThumbnails };
