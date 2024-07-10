022d316183477a6f7860678e358d07aeb88405c7
Keks <keks@htmlacademy.ru>
2024-07-10 12:02:19 +0300

    :heavy_check_mark: Сборка #7

---
+++ 7/js/thumbnails.js
@@ -0,0 +1,42 @@
+import { createArrayOfObjects } from './util.js';
+import { getPhotoObj } from './data.js';
+import { openBigPictureModal } from './bigPicture.js';
+
+const gallery = document.querySelector('.pictures');
+const thumbnailTemplate = document
+  .querySelector('#picture')
+  .content
+  .querySelector('.picture');
+
+let thumbnailsArray;
+
+const showThumbnails = (amount) => {
+  thumbnailsArray = createArrayOfObjects(amount, getPhotoObj);
+  const thumbnailsGalleryFragment = document.createDocumentFragment();
+
+  thumbnailsArray.forEach(({id, url, description, likes, comments}) => {
+    const newThumbnail = thumbnailTemplate.cloneNode(true);
+
+    newThumbnail.id = id;
+
+    const picture = newThumbnail.querySelector('.picture__img');
+    picture.src = url;
+    picture.alt = description;
+
+    newThumbnail.querySelector('.picture__likes').textContent = likes;
+    newThumbnail.querySelector('.picture__comments').textContent = comments.length;
+
+    thumbnailsGalleryFragment.append(newThumbnail);
+  });
+
+  gallery.append(thumbnailsGalleryFragment);
+};
+
+gallery.addEventListener('click', (evt) => {
+  if (evt.target.matches('img')) {
+    const pictureId = Number(evt.target.closest('a').id);
+    openBigPictureModal(thumbnailsArray[pictureId - 1]);
+  }
+});
+
+export { showThumbnails };
---
+++ 7/js/util.js
@@ -0,0 +1,28 @@
+const getRandomNumber = (min, max) => Math.floor(Math.random() * (max - min + 1) + min);
+
+const getRandomItemFromArray = (array) => array[getRandomNumber(0, array.length - 1)];
+
+const createArrayOfObjects = (length, objConstructor) => Array.from({length}, objConstructor);
+
+const getId = () => {
+  let i = 1;
+  return function () {
+    return i++;
+  };
+};
+
+const getUrl = (folder, format) => function (title) {
+  return `${folder}/${title}.${format}`;
+};
+
+const isEscapeKey = (evt) => evt.code === 'Escape';
+
+
+export {
+  getRandomNumber,
+  getRandomItemFromArray,
+  createArrayOfObjects,
+  getId,
+  getUrl,
+  isEscapeKey
+};
