d6bc0bac02ca8b385457215f76cd22a095a27151
Keks <keks@htmlacademy.ru>
2024-07-14 15:11:12 +0300

    :heavy_check_mark: Сборка #8

---
+++ 8/js/main.js
@@ -0,0 +1,6 @@
+import { thumbnailsArray } from './data.js';
+import { showThumbnails, galleryListener } from './thumbnails.js';
+
+showThumbnails(thumbnailsArray);
+galleryListener(thumbnailsArray);
+
---
+++ 8/js/thumbnails.js
@@ -0,0 +1,45 @@
+import { openBigPictureModal } from './bigPicture.js';
+
+const gallery = document.querySelector('.pictures');
+const thumbnailTemplate = document
+  .querySelector('#picture')
+  .content
+  .querySelector('.picture');
+
+const showThumbnails = (thumbnailsArray) => {
+  const thumbnailsGalleryFragment = document.createDocumentFragment();
+
+  thumbnailsArray.forEach(({id, url, description, likes, comments}) => {
+    const newThumbnail = thumbnailTemplate.cloneNode(true);
+
+    newThumbnail.dataset.photoId = id;
+
+    const picture = newThumbnail.querySelector('.picture__img');
+    picture.src = url;
+    picture.alt = description;
+
+    newThumbnail.querySelector('.picture__likes').textContent = likes;
+    newThumbnail.querySelector('.picture__comments').textContent = comments.length;
+
+    thumbnailsGalleryFragment.append(newThumbnail);
+  });
+
+  gallery.append(thumbnailsGalleryFragment);
+};
+
+const galleryListener = (thumbnailsArray) => {
+  gallery?.addEventListener('click', (evt) => {
+    evt.preventDefault();
+
+    if (evt.target.matches('img')) {
+      const photoId = Number(evt.target.closest('a').dataset.photoId);
+      openBigPictureModal(thumbnailsArray.find((el) => el.id === photoId));
+    }
+  });
+};
+
+
+export {
+  showThumbnails,
+  galleryListener
+};
