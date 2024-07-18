221fd63df19099ae5cddc4dc68a221c7d8441bd9
Mila <darlingmila@yandex.ru>
2024-07-18 17:03:32 +0300

    Добавляет в форму возможность масштабировать фото и добавлять эффект

--- js/form.js
+++ js/form.js
@@ -1,4 +1,5 @@
 import '../vendor/pristine/pristine.min.js';
+import '../vendor/nouislider/nouislider.js';
 
 import { isEscapeKey, isNotFormInput } from './util.js';
 
@@ -6,20 +7,33 @@ const MAX_HASHTAGS_COUNT = 5;
 const MAX_HASHTAG_LENGTH = 20;
 const MAX_DESCRIPTION_LENGTH = 140;
 
+const SCALE_STEP = 25;
+
 const form = document.querySelector('.img-upload__form');
 const imgInput = document.querySelector('.img-upload__input');
 const closeBtn = document.querySelector('.img-upload__cancel');
 
-// const imgPreview = document
-//   .querySelector('.img-upload__preview')
-//   .querySelector('img');
+const imgScaleFieldset = document.querySelector('.img-upload__scale');
+const scaleSmallerBtn = imgScaleFieldset.querySelector('.scale__control--smaller');
+const scaleBiggerBtn = imgScaleFieldset.querySelector('.scale__control--bigger');
+const imgScaleInput = imgScaleFieldset.querySelector('.scale__control--value');
+
+const imgPreview = document
+  .querySelector('.img-upload__preview')
+  .querySelector('img');
 // const smallPreviews = document.querySelectorAll('.effects__preview');
+const effectsList = document.querySelector('.effects__list');
+
 
 const hashtagsInput = document.querySelector('.text__hashtags');
 const descriptionInput = document.querySelector('.text__description');
 
 const overlay = document.querySelector('.img-upload__overlay');
 
+const effectLevelBox = document.querySelector('.img-upload__effect-level');
+const effectLevelValue = document.querySelector('.effect-level__value');
+
+const effectSlider = document.querySelector('.effect-level__slider');
 
 const hashtagReg = /^#[а-яёa-z0-9]{1,19}$/;
 
@@ -36,8 +50,93 @@ const errorMessage = {
   description: 'Длина комментария не может составлять больше 140 символов.'
 };
 
+const effectsData = {
+  chrome: {
+    effectsObj : {
+      range: {
+        min: 0,
+        max: 1,
+      },
+      step: 0.1,
+      start: 1,
+    },
+    filter: 'grayscale',
+    unit: ''
+  },
+  sepia: {
+    effectsObj : {
+      range: {
+        min: 0,
+        max: 1,
+      },
+      step: 0.1,
+      start: 1,
+    },
+    filter: 'sepia',
+    unit: ''
+  },
+  marvin: {
+    effectsObj : {
+      range: {
+        min: 0,
+        max: 100,
+      },
+      step: 1,
+      start: 100,
+    },
+    filter: 'invert',
+    unit: '%'
+  },
+  phobos: {
+    effectsObj : {
+      range: {
+        min: 1,
+        max: 3,
+      },
+      step: 0.1,
+      start: 3,
+    },
+    filter: 'blur',
+    unit: 'px'
+  },
+  heat: {
+    effectsObj : {
+      range: {
+        min: 1,
+        max: 3,
+      },
+      step: 0.1,
+      start: 3,
+    },
+    filter: 'brightness',
+    unit: ''
+  },
+  none: {
+    effectsObj : {
+      range: {
+        min: 0,
+        max: 1,
+      },
+      step: 0.1,
+      start: 1,
+    }
+  }
+};
+
 
 let pristine;
+let currentImgEffect = 'none';
+
+
+noUiSlider.create(effectSlider, {
+  range: {
+    min: 0,
+    max: 1,
+  },
+  step: 0.1,
+  start: 1,
+  connect: 'lower'
+});
 
 
 const validateHashtag = (value) => {
@@ -107,12 +206,15 @@ const addValidators = () => {
 };
 
 const openUploadImgModal = () => {
+  // imgScaleInput.value = `100%`;
   //-----
   // imgPreview.src = '../img/logo-background-2.jpg';
   // smallPreviews.forEach((preview) => {
   //   preview.style.backgroundImage = 'url(../img/logo-background-2.jpg)';
   // });
   //-----
+  currentImgEffect = 'none';
+  effectSlider.noUiSlider.set();
 
   document.activeElement.blur();
 
@@ -144,10 +246,10 @@ function onFormSubmit (evt) {
   const isValid = pristine.validate();
 
   if (isValid) {
-    // console.log('Можно отправлять');
+    console.log('Можно отправлять');
     closeUploadImgModal();
   } else {
-    // console.log('Форма невалидна');
+    console.log('Форма невалидна');
   }
 }
 
@@ -165,3 +267,77 @@ closeBtn.addEventListener('click', closeUploadImgModal);
 
 
 export { imgInputListener };
+
+
+
+
+
+
+
+const getScaleValueAsNumber = () => Number.parseInt(imgScaleInput.value, 10);
+
+const makeImgSmaller = () => {
+  const currentValue = getScaleValueAsNumber();
+  const newValue = currentValue <= SCALE_STEP ? SCALE_STEP : currentValue - SCALE_STEP;
+
+  imgScaleInput.value = `${newValue}%`;
+  // changeImgScale();
+};
+
+const makeImgBigger = () => {
+  const currentValue = getScaleValueAsNumber();
+  const newValue = currentValue > 100 - SCALE_STEP ? 100 : currentValue + SCALE_STEP;
+
+  imgScaleInput.value = `${newValue}%`;
+  // changeImgScale();
+};
+
+const changeImgScale = () => {
+  const value = getScaleValueAsNumber();
+  console.log(value / 100);
+  imgPreview.style.transform = `scale(${value / 100})`;
+};
+
+scaleSmallerBtn.addEventListener('click', makeImgSmaller);
+scaleBiggerBtn.addEventListener('click', makeImgBigger);
+
+imgScaleInput.addEventListener('input', changeImgScale);
+
+
+
+
+
+
+
+
+
+
+const changeEffect = (evt) => {
+  const effect = evt.target.value;
+
+  const newOptions = effectsData[effect].effectsObj;
+  currentImgEffect = effect;
+
+  effectSlider.noUiSlider.updateOptions(newOptions);
+};
+
+effectSlider.noUiSlider.on('update', () => {
+  const value = effectSlider.noUiSlider.get();
+
+  if (currentImgEffect === 'none') {
+    imgPreview.style.filter = 'none';
+    effectLevelValue.value = '';
+    effectLevelBox.classList.add('hidden');
+
+  } else {
+    const filter = effectsData[currentImgEffect].filter;
+    const unit = effectsData[currentImgEffect].unit;
+
+    imgPreview.style.filter = `${filter}(${value}${unit})`;
+    effectLevelValue.value = value;
+    effectLevelBox.classList.remove('hidden');
+  }
+});
+
+effectsList.addEventListener('change', changeEffect);
+
